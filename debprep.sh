fcd90f263e3b25c814463b4a70d66a00448dfde3
Dallas Moore <Dallas.moore21@gmail.com>
2014-09-25 11:51:21 -0500

    Update debprep.sh

--- debprep.sh
+++ debprep.sh
@@ -1,13 +1,13 @@
 echo -n "" > /etc/apt/sources.list
 
-echo -n "# Main package source" > /etc/apt/sources.list
-echo -n "deb http://ftp.us.debian.org/debian stable main non-free" > /etc/apt/sources.list
-echo -n "deb-src http://ftp.us.debian.org/debian stable main non-free" > /etc/apt/sources.list
-echo -n "# Main update source" > /etc/apt/sources.list
-echo -n "deb http://ftp.debian.org/debian/ wheezy-updates main non-free" > /etc/apt/sources.list
-echo -n "deb-src http://ftp.debian.org/debian/ wheezy-updates main non-free" > /etc/apt/sources.list
-echo -n "# Main security source" > /etc/apt/sources.list
-echo -n "deb http://security.debian.org/ wheezy/updates main non-free" > /etc/apt/sources.list
-echo -n "deb-src http://security.debian.org/ wheezy/updates main non-free" > /etc/apt/sources.list
+echo -n "# Main package source" >> /etc/apt/sources.list
+echo -n "deb http://ftp.us.debian.org/debian stable main non-free" >> /etc/apt/sources.list
+echo -n "deb-src http://ftp.us.debian.org/debian stable main non-free" >> /etc/apt/sources.list
+echo -n "# Main update source" >> /etc/apt/sources.list
+echo -n "deb http://ftp.debian.org/debian/ wheezy-updates main non-free" >> /etc/apt/sources.list
+echo -n "deb-src http://ftp.debian.org/debian/ wheezy-updates main non-free" >> /etc/apt/sources.list
+echo -n "# Main security source" >> /etc/apt/sources.list
+echo -n "deb http://security.debian.org/ wheezy/updates main non-free" >> /etc/apt/sources.list
+echo -n "deb-src http://security.debian.org/ wheezy/updates main non-free" >> /etc/apt/sources.list
 
 apt-get update && apt-get install sudo build-essential linux-headers-$(uname -r)
