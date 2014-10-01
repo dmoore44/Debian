bc2f51fad3d287f6cd4754d909ce2ef04035b02b
Dallas Moore <Dallas.moore21@gmail.com>
2014-10-01 10:15:59 -0500

    Update debprep.sh

--- debprep.sh
+++ debprep.sh
@@ -18,4 +18,12 @@ echo -n "deb http://security.debian.org/ wheezy/updates main non-free" >> /etc/a
 echo -n "" >> /etc/apt/sources.list
 echo -n "deb-src http://security.debian.org/ wheezy/updates main non-free" >> /etc/apt/sources.list
 
+echo -n "export https_proxy=$http_proxy" >> /etc/bash.bashrc
+echo -n "" >> /etc/bash.bashrc
+echo -n "export ftp_proxy=$http_proxy" >> /etc/bash.bashrc
+echo -n "" >> /etc/bash.bashrc
+echo -n "export rsync_proxy=$http_proxy" >> /etc/bash.bashrc
+echo -n "" >> /etc/bash.bashrc
+echo -n 'export no_proxy="localhost,127.0.0.1,localaddress,.localdomain.com"' >> /etc/bash.bashrc
+
 apt-get update && apt-get install sudo build-essential linux-headers-$(uname -r)
